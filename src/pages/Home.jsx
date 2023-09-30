de7d04be9f55a227164e988a72ccdb1c86e44000
swastik <swastikasija072@gmail.com>
2023-09-30 15:07:22 +0530

    css added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,25 @@ function Home() {
   const [value, setValue] = useState();
   const navigate = useNavigate();
   const handleJoinRoom = useCallback(() => {
-    navigate(`/room/${value}`)
+    navigate(`/room/${value}`);
   }, [navigate, value]);
   return (
     <>
-      <h1>Home Page</h1>
-      <input
-        type="text"
-        placeholder="enter room id"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button onClick={handleJoinRoom}>Join</button>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">
+          Password
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputPassword1"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <button type="submit" class="btn btn-outline-success" onClick={handleJoinRoom}>
+        Join
+      </button>
     </>
   );
 }
